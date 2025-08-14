3c40e13852916a32013ee822a000c324d4f360c3
miguel martinez <miguelangelmartinezm21@gmail.com>
2025-08-13 21:28:28 -0500

    tarea miguel

---
+++ actividad1/src/actividad1/Actividad1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad1;
+
+import java.util.Scanner;
+
+
+public class Actividad1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Introdusca La Nota 1: ");
+        double N1 = sc.nextDouble();
+        System.out.println("Introdusca La Nota 2: ");
+        double N2 = sc.nextDouble();
+        System.out.println("Introdusca La Nota 3: ");
+        double N3 = sc.nextDouble();
+        double NF = (N1*0.30)+(N2*0.30)+(N3*0.40);
+         if (NF<=1.9){
+        System.out.println("La Nota es deficiente ");}
+        else if (NF>=2 && NF<=2.9){
+        System.out.println("La Nota Es Insuficiente ");}
+        else if (NF>=3 && NF<=3.9){
+        System.out.println("La Nota Es Aceptable ");}
+         else if (NF>=4 && NF==5){
+        System.out.println("La Nota Es Destacado ");}
+        System.out.println("La Nota Final Es: "+NF);  
+    }
+    
+}

07582b0f38e0c519472084f908b5ad7874b88e19
yojansalamanca33 <yogansalamanca1234@gmail.com>
2025-08-25 17:44:21 -0500

    Tarea Primera GUI.

---
+++ Formulario/src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author aylee
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario FR = new Formulario();
+        FR.setVisible(true);
+    }
+   
+    
+}
